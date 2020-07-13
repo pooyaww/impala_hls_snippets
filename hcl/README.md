0063712db44106a8b8240b8c3327a10011217ac9
Puya Amiri <puya.amiri@dfki.de>
2020-07-13 16:17:15 +0200

    test cases for HLS with opencl memory allocations

---
+++ hcl/README.md
@@ -0,0 +1,1 @@
+Introducing HLS code with OpenCL memorry allocations
---
+++ hcl/test_hcl_host.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+name=$1
+
+if [ -z "$1" ]; then
+    echo "No test name supplied"
+    exit
+fi
+
+cd build
+
+#clean up
+if ls ./*${name}*.{dump,cpp} 1> /dev/null 2>&1; then
+    echo "cleaning up..."
+    rm *
+fi
+
+echo "Re-compiling and re-generating..."
+impala ~/anydsl/runtime/platforms/*.impala ~/anydsl/runtime/src/*.impala ../${name}.impala -emit-llvm > hls_host_ir_${name}.dump
+
+vim -O ${name}.ll hls_host_ir_${name}.dump
+cd ..
