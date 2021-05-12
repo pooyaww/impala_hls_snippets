48f3806170563e3f501aabbbea58df8e43d21b7b
Puya Amiri <puya.amiri@dfki.de>
2021-05-12 22:26:40 +0200

    adding more tests, Artic compatibility

---
+++ hcl/artic/test_hcl_device_host_code.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+name=$1
+
+if [ -z "$1" ]; then
+    echo "No test name supplied"
+    exit
+fi
+
+cd build
+
+#clean up
+if ls ./*${name}*.{dump,cpp} 1> /dev/null 2>&1; then
+    echo "cleaning up..."
+    rm *
+fi
+
+echo "Re-compiling and re-generating..."
+artic ~/anydsl/runtime/platforms/artic/*.impala ../${name}.impala --emit-llvm -o ${name} > hls_host_ir_${name}.dump
+
+mv ${name}.hls ${name}.cpp
+
+vim -O ../${name}.impala ${name}.ll hls_host_ir_${name}.dump ${name}.cpp
+cd ..
