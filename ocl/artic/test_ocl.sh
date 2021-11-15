695ac4fbf9a3f457a8c2e8727a2a0cbd7a72b7c6
pooyaww <pooya.w@gmail.com>
2021-11-15 19:49:17 +0100

    artic for opencl

---
+++ ocl/artic/test_ocl.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+name=$1
+
+if [ -z "$1" ]; then
+    echo "No test name supplied"
+    exit
+fi
+
+cd build
+
+#clean up
+if ls ./*${name}*.{dump,cpp} 1> /dev/null 2>&1; then
+    echo "cleaning up..."
+    rm *
+fi
+
+echo "Re-compiling and re-generating..."
+artic  ~/Projects/anydsl/runtime/platforms/artic/*.impala ../${name}.art > hls_ir_${name}.dump --emit-llvm -o ${name}
+if [ $2 == "d" ]; then
+    echo ${name}
+    mv ${name}.cl ${name}_cl.cpp
+    vim -O ${name}_cl.cpp hls_ir_${name}.dump
+elif [ $2 == "h" ]; then
+    vim -O ${name}.ll hls_ir_${name}.dump
+else
+    echo "ERROR!
+        d --> Device cose
+        h --> Host code"
+fi
+cd ..
