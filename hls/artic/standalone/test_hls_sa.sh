c40f4601e6496fdec809f698ba86923cd535dd20
Puya Amiri <pooya.w@gmail.com>
2022-04-12 20:19:17 +0200

    artic standalone tests

---
+++ hls/artic/standalone/test_hls_sa.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+basename=$1
+
+name="${basename%.*}"
+echo $name
+
+if [[ -v INTERFACE ]]; then
+    intfc="${INTERFACE}"
+elif [[ -v ANYDSL_FPGA ]]; then
+    intfc="${ANYDSL_FPGA}"
+else
+    intfc="HPC"
+fi
+
+if [ -z "$1" ]; then
+    echo "No test name supplied"
+    exit
+fi
+
+cd build
+
+#clean up
+if ls ./*${name}*.{dump,cpp} 1> /dev/null 2>&1; then
+    echo "cleaning up..."
+    rm *
+fi
+
+echo "Re-compiling and re-generating..."
+artic --hls-flags ${intfc} ../${name}.art --emit-llvm -o ${name} > hls_host_ir_${name}.dump
+if [ $2 == "d" ] && [ -z "$3" ] && [ -z "$4" ]; then
+    mv ${name}.hls ${name}.cpp
+    vim -O hls_host_ir_${name}.dump ${name}.cpp
+elif [ $2 == "h" ] && [ -z "$3" ] && [ -z "$4" ]; then
+    vim -O ${name}.ll hls_host_ir_${name}.dump
+elif [ $2 == "d" ] && [ $3 == "h" ] && [ -z "$4" ]; then
+    mv ${name}.hls ${name}.cpp
+    vim -O ${name}.ll hls_host_ir_${name}.dump ${name}.cpp
+elif [ $2 == "d" ] && [ $3 == "h" ] && [ $4 == "c" ]; then
+    mv ${name}.hls ${name}.cpp
+    vim -O ../${name}.impala ${name}.ll hls_host_ir_${name}.dump ${name}.cpp
+else
+    echo "ERROR!
+    d --> decvice
+    h --> host
+    c --> source code"
+fi
+
+cd ..
