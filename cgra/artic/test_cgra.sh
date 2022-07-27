f4b5ece354f8930539489ab26d0f8a591ce68352
Puya Amiri <pooya.w@gmail.com>
2022-07-27 18:12:02 +0200

    update compile scripts

--- cgra/artic/test_cgra.sh
+++ cgra/artic/test_cgra.sh
@@ -31,18 +31,18 @@ if ls ./*${name}*.{dump,cpp} 1> /dev/null 2>&1; then
 fi
 
 echo "Re-compiling and re-generating..."
-artic --hls-flags ${intfc} ${ANYDSL_INSTALL}/runtime/platforms/artic/*.impala ../${name}.art --emit-llvm -o ${name} > hls_host_ir_${name}.dump
+artic --hls-flags ${intfc} ${ANYDSL_INSTALL}/runtime/platforms/artic/*.impala ../${name}.art --emit-llvm -o ${name} > cgra_host_ir_${name}.dump
 if [ $2 == "d" ] && [ -z "$3" ] && [ -z "$4" ]; then
-    mv ${name}.hls ${name}.cpp
-    vim -O hls_host_ir_${name}.dump ${name}.cpp
+    mv ${name}.cgra ${name}.cpp
+    vim -O cgra_host_ir_${name}.dump ${name}.cpp
 elif [ $2 == "h" ] && [ -z "$3" ] && [ -z "$4" ]; then
-    vim -O ${name}.ll hls_host_ir_${name}.dump
+    vim -O ${name}.ll cgra_host_ir_${name}.dump
 elif [ $2 == "d" ] && [ $3 == "h" ] && [ -z "$4" ]; then
-    mv ${name}.hls ${name}.cpp
-    vim -O ${name}.ll hls_host_ir_${name}.dump ${name}.cpp
+    mv ${name}.cgra ${name}.cpp
+    vim -O ${name}.ll cgra_host_ir_${name}.dump ${name}.cpp
 elif [ $2 == "d" ] && [ $3 == "h" ] && [ $4 == "c" ]; then
-    mv ${name}.hls ${name}.cpp
-    vim -O ../${name}.impala ${name}.ll hls_host_ir_${name}.dump ${name}.cpp
+    mv ${name}.cgra ${name}.cpp
+    vim -O ../${name}.impala ${name}.ll cgra_host_ir_${name}.dump ${name}.cpp
 else
     echo "ERROR!
     d --> decvice
